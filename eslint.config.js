6771db36d8abb14f1148ba8ac1bda4f356ef97df
yinm <yinm.cminor9@gmail.com>
2024-09-08 11:55:30 +0900

    Setup ESLint

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import yinmBase from "eslint-config-yinm";
+import yinmTypeScript from "eslint-config-yinm/typescript";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [...yinmBase, ...yinmTypeScript];
